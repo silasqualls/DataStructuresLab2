3a03130b00213851802acb57671915ac1c40a104
jbredwards <143640517+JulianUSM@users.noreply.github.com>
2023-10-01 21:24:53 -0400

    implement add & remove methods + fix bugs
    myArayList:
    - neater whitespace
    - add a getModCount method that's used as an accessor by myListIterator
    myListIterator:
    - implement add & remove methods (not necessary for the assignment, but at first I thought I was gonna use them for addAll & removeAllNext, turns out this wasn't the case but I see no point in removing the logic now...)
    - some small optimizations for addAll & removeAllNext
    - remove myListIterator local modCount field (and replace it with an accessor to the myArrayList's modCount field)

--- myArrayList.java
+++ myArrayList.java
@@ -1,29 +1,37 @@
+import java.util.ArrayList;
 
+public class myArrayList extends ArrayList<Fraction>
+{
+    public myArrayList() {
+        ensureCapacity(50);
+    }
 
-    import java.util.ArrayList;
-  public class myArrayList extends ArrayList<Fraction> {
+    public Fraction remove(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException("Index is out of bounds.");
+        }
 
+        return super.remove(index);
+    }
 
-      public myArrayList() {
-          ensureCapacity(50);
-      }
-      public Fraction remove(int index) {
-          if (index < 0 || index >= size()) {
-              throw new IndexOutOfBoundsException("Index is out of bounds.");
-          }
+    public void add(int index, Fraction input) {
+        if (index < 0 || index > size()) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+        ensureCapacity(size() + 1);
+        super.add(index, input);
+    }
 
-          return super.remove(index);
-      }
+    public int indexOf(final Fraction input) {
+        return super.indexOf(input);
+    }
 
-      public void add(int index, Fraction input) {
-          if (index < 0 || index > size()) {
-              throw new IndexOutOfBoundsException("Index is out of bounds");
-          }
-          ensureCapacity(size() + 1);
-          super.add(index, input);
-      }
-      public int indexOf(Fraction input) {
-          return super.indexOf(input);
-      }
-  }
-
+    /**
+     * Expose this value, so it can be used to prevent possible desyncs from {@link myListIterator}.
+     * @return {@link ArrayList#modCount}.
+     *
+     * @since 1 October 2023
+     * @author Julian Edwards
+     */
+    int getModCount() { return modCount; }
+}
