99583f2a6b56be2131763488f79fe5b1ef05c1b4
silasqualls <144641458+silasqualls@users.noreply.github.com>
2023-10-02 13:43:17 -0400

    Update lab2main.java

--- lab2main.java
+++ lab2main.java
@@ -12,23 +12,27 @@ public class lab2main {
         
         myArrayList.myListIterator itr = arrayList1.getIterator();
      
-        Fraction[] fracArray = new Fraction[]{new Fraction(1,2), new Fraction(4,5)};  
+        Fraction[] fracArray = new Fraction[]{new Fraction(1,2), new Fraction(4,6)};  
         //adds all the Fractions from an array to ArrayList
+        
         itr.addAll(fracArray);
         //Checks if there is a next element and prints it and moves our cursor
         //until we reach end
+        System.out.println("Next Prints: ");
         while(itr.hasNext()) {
-            System.out.println(itr.next());
+            itr.next().print();;
         }
         //Checks for previous element and prints it and moves cursor back
         //until we reach beginning
+        System.out.println("Previous Prints: ");
         while(itr.hasPrevious()) {
-            System.out.println(itr.previous());
+            itr.previous().print();
         }
         itr.removeAllNext();
-        //Should have removed all elements from arrayList
+        //Should have removed all elements from arrayList except for the first element
+        System.out.println("Remove Prints: ");
         while(itr.hasNext()) {
-            System.out.println(itr.next());
+            itr.next().print();;
         }
     }
 
